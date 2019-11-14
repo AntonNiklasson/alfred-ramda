3703ce0c8d0c81027db3ca4f5742073a14cdf79e
Anton Niklasson <anton.niklasson@klarna.com>
2019-11-14 20:38:23 +0100

    almost working now

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const alfy = require('alfy');
-const cheerio = require('cheerio')
+const alfy = require("alfy");
+const cheerio = require("cheerio");
 
+(async () => {
+  const response = await alfy.fetch("https://ramdajs.com/docs/", {
+    json: false
+  });
+  const $ = cheerio.load(response.match(/<body[^>]*>((.|[\n\r])*)<\/body>/)[0]);
 
-async (() => {
-	const response = await alfy.fetch('https://ramdajs.com/docs/')
+  alfy.log($.html());
 
-	$ = cheerio.load(response.body)
-	alfy.log($.html())
-})()
+  let items = [];
+
+  $("ul.toc .func a").each((index, element) => {
+    items.push(
+      $(element)
+        .text()
+        .trim()
+        .split("\n")[0]
+    );
+  });
+
+  alfy.log(items);
+
+  alfy.output(items);
+})();
